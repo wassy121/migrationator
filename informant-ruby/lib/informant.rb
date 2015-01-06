5f4bf885a107bfdff92ece97f5a790dca82d163d
Samuel Stringham <samuel.stringham@gmail.com>
2015-01-06 06:43:35 -0500

    make logging less boastful

--- informant-ruby/lib/informant.rb
+++ informant-ruby/lib/informant.rb
@@ -13,20 +13,21 @@ class Informant
     @log.level = Logger::DEBUG
     @dpkg_files = []
     @rpm_files = []
+    @dpkg_list = []
+    @rpm_list = []
     @unpackaged_files = []
   end
 
   def gather_facts
     determine_pkg_managers().each do |mgr|
-      puts mgr
       self.send(:"gather_#{mgr}_files")
+      @log.debug "Found" + (eval "@#{mgr}_files.count").to_s + " dpkg files"
     end
-    @log.info "Found #{@dpkg_files.count} dpkg files"
     @log.info "Found #{@rpm_files.count} rpm files"
-    all_files = gather_all_files()
+    all_files = [] #gather_all_files()
     @unpackaged_files = all_files -= @dpkg_files -= @rpm_files
-    @log.info "Found #{@unpackaged_files.count} unpackaged files"
-    @log.info "All files counted were #{all_files.count}"
+    @log.debug "Found #{@unpackaged_files.count} unpackaged files"
+    @log.debug "All files counted were #{all_files.count}"
     #gather_dpkg_files()
     #gather_rpm_files()
     #gather_unpackaged_files(pkg_managers)
@@ -53,13 +54,19 @@ class Informant
   end
 
   def gather_dpkg_files
-    @log.info "Starting dpkg search"
-    require 'facts/dpkg'
+    @log.debug "Starting dpkg search"
+    require 'vendor/debian'
+    puts Debian::Dpkg::STATUS_FILE
+    thr = Thread.new do
+      @dpkg_list  = Debian::Packages.new(Debian::Dpkg::STATUS_FILE,   [".*"],[])
+    end
+    @log.debug "found #{@dpkg_list.count} debian packages"
   end
 
   def gather_rpm_files
-    @log.info "Starting rpm search"
-    require 'facts/rpm'
+    @log.debug "Starting rpm search"
+    #require 'facts/rpm'
+    #return Rpm.get_files()
   end
 
   def determine_pkg_managers()
