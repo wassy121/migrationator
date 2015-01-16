77effe4078fb09ed21669069d43674efb9224ae6
Samuel Stringham <samuel.stringham@gmail.com>
2015-01-15 20:01:00 -0500

    Amending the user model, and application setup for new devise version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :recoverable
+  devise :database_authenticatable, :recoverable,
          :rememberable, :registerable, :trackable, :timeoutable, :validatable
 
   has_many :servers, :dependent => :destroy
